5289a6bac2a0292d36811b8ca88579b016977519
Saja Malkawi <sajamalkawi95@gmail.com>
2021-08-08 14:16:50 +0300

    genrate folders

---
+++ script/script.js
@@ -0,0 +1,3 @@
+
+let imgesSrc = [ 'bag.jpg' , 'banana.jpg' , 'bathroom.jpg' , 'boots.jpg' , 'breakfast.jpg' , 'bubblegum.jpg', 'chair.jpg' , 'cthulhu.jpg' , 'dog-duck.jpg' , 'dragon.jpg' , 'pen.jpg' , 'pet-sweep.jpg' , 'scissors.jpg' , 'shark.jpg' , 'sweep.png' , 'tauntaun.jpg' , 'unicorn.jpg' , 'water-can.jpg' , 'wine-glass.jpg'];
+
